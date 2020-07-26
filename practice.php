cec04ab486af123d12e1ab48a827f7e7196f178a
teruaki niikawa <n.teruaki.2301@icloud.com>
2020-07-26 04:39:30 +0000

    課題提出

--- practice.php
+++ practice.php
@@ -14,6 +14,26 @@ for($i = 0; $i <= 10000; $i++){
 echo $total;
 
 $fruits = array("apple","orange","lemon","grape","pine");
+foreach ($fruits as $fruit){
+    echo "要素は" . $fruit;
+    echo "\n";
+}
+
+$name = "新川輝明";
+if ($name == "新川輝明"){
+    echo "私は新川輝明です";
+}else{
+    echo "あなたの名前ではありません";
+}
+
+$total = 0;
+echo $total;
+for($i = 0; $i <= 10000; $i++){
+    $total += $i;
+}
+echo $total;
+
+$fruits = array("apple","orange","lemon","grape","pine");
 for ($i = 0; $i < count($fruits); $i++){
     echo "要素は" .$fruits[$i];
     echo "\n";
@@ -27,5 +47,3 @@ for($i = $start; $i < $end; $i++){
   if($i % 5 == 0)
     echo $i
 }
-
-  
